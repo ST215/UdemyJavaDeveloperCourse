c0cc2b0ecd9b01c8153694020375e2fdf49196d7
Stanley H Griggs II <shg5004@gmail.com>
2017-03-22 06:32:38 -0400

    Created new class VipCustomer with 3 fields

---
+++ Constructors/src/com/stanleygriggs/VipCustomer.java
@@ -0,0 +1,11 @@
+package com.stanleygriggs;
+
+/**
+ * Created by Stanley on 3/22/17.
+ */
+public class VipCustomer {
+    private String name;
+    private double creditLimit;
+    private String emailAddress;
+    
+}
