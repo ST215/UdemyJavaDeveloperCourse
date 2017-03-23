418a42e62f8bbf9c41a7c240f9917c5cf9ccbc64
Stanley H Griggs II <shg5004@gmail.com>
2017-03-22 21:10:07 -0400

    Fixed account withdrawal bug

--- Constructors/src/com/stanleygriggs/Main.java
+++ Constructors/src/com/stanleygriggs/Main.java
@@ -15,17 +15,19 @@ public class Main {
 //
 //        bobsAccount.deposit(51.0);
 //        bobsAccount.withdrawal(100.0);
+
+        Account timsAccount = new Account("Tim", "tim@email.com", "12345");
+        System.out.println(timsAccount.getNumber() + " name " + timsAccount.getCustomerName());
+        System.out.println("Current balance is " + timsAccount.getBalance());
+        timsAccount.withdrawal(100.55);
+
+//        VipCustomer person1 = new VipCustomer();
+//        System.out.println(person1.getName());
 //
-//        Account timsAccount = new Account("Tim", "tim@email.com", "12345");
-//        System.out.println(timsAccount.getNumber() + " name " + timsAccount.getCustomerName());
-
-        VipCustomer person1 = new VipCustomer();
-        System.out.println(person1.getName());
-
-        VipCustomer person2 = new VipCustomer("Bob", 25000.00);
-        System.out.println(person2.getName());
-
-        VipCustomer person3 = new VipCustomer("Tim", 100.00, "tim@email.com");
-        System.out.println(person3.getName());
+//        VipCustomer person2 = new VipCustomer("Bob", 25000.00);
+//        System.out.println(person2.getName());
+//
+//        VipCustomer person3 = new VipCustomer("Tim", 100.00, "tim@email.com");
+//        System.out.println(person3.getName());
     }
 }
