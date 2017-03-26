8335583938076d0dec7e83e1061b790fc7fb426f
Stanley H Griggs II <shg5004@gmail.com>
2017-03-26 13:01:54 -0400

    Completed Inheritance

--- InheritanceChallenge/src/Main.java
+++ InheritanceChallenge/src/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Porsche porsche = new Porsche(36);
+        porsche.steer(45);
+        porsche.accelerate(30);
+        porsche.accelerate(20);
+        porsche.accelerate(-42);
 
     }
 }
